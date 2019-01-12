e8b6978b6ba087e28570223e9238848feb825bd9
Geanina Cosovanu <cosovanugeanina@gmail.com>
2019-01-12 11:25:54 +0200

    read image

---
+++ CudaSample/main.cpp
@@ -0,0 +1,43 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector> // for vector
+#include <iterator> // for std::istream_iterator and std::ostream_iterator
+#include <algorithm> // for std::copy
+using namespace std;
+using namespace cv;
+
+
+
+void readImageOpenCV() {
+	Mat img = imread("flower.jpg");
+	namedWindow("image", WINDOW_NORMAL);
+	imshow("image", img);
+	waitKey(0);
+}
+
+
+void readImage() {
+	ifstream file("flower.jpg", ios::binary);
+	vector<unsigned char> buffer(istreambuf_iterator<char>(file), {});
+}
+int main() {
+
+	// Define file stream object, and open the file
+	ifstream inputFile("flower.jpg", ios::binary);
+	ofstream outputFile("flower1.jpg", ios::binary);
+
+
+	// Reading the file content using the iterator!
+	
+	std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(inputFile), {});
+
+	
+	copy(buffer.begin(), buffer.end(), ostreambuf_iterator<char>(outputFile));
+
+
+	cout << "Done";
+	return 0;
+}
+
+
