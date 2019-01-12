cf73e297ef14acc0fcad4da4696955b62e5ea294
Bogdan Demian <ds.bogdan7@gmail.com>
2019-01-12 15:04:05 +0200

    cuda

--- CudaSample/kernel.cu
+++ CudaSample/kernel.cu
@@ -1,121 +1,139 @@
-//
-//#include "cuda_runtime.h"
-//#include "device_launch_parameters.h"
-//
-//#include <stdio.h>
-//
-//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-//
-//__global__ void addKernel(int *c, const int *a, const int *b)
-//{
-//	int i = threadIdx.x;
-//	c[i] = a[i] + b[i];
-//}
-//
-//int main()
-//{
-//	const int arraySize = 5;
-//	const int a[arraySize] = { 1, 2, 3, 4, 5 };
-//	const int b[arraySize] = { 10, 20, 30, 40, 50 };
-//	int c[arraySize] = { 0 };
-//
-//	// Add vectors in parallel.
-//	cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "addWithCuda failed!");
-//		return 1;
-//	}
-//
-//	printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-//		c[0], c[1], c[2], c[3], c[4]);
-//
-//	// cudaDeviceReset must be called before exiting in order for profiling and
-//	// tracing tools such as Nsight and Visual Profiler to show complete traces.
-//	cudaStatus = cudaDeviceReset();
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaDeviceReset failed!");
-//		return 1;
-//	}
-//
-//	return 0;
-//}
-//
-//// Helper function for using CUDA to add vectors in parallel.
-//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-//{
-//	int *dev_a = 0;
-//	int *dev_b = 0;
-//	int *dev_c = 0;
-//	cudaError_t cudaStatus;
-//
-//	// Choose which GPU to run on, change this on a multi-GPU system.
-//	cudaStatus = cudaSetDevice(0);
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-//		goto Error;
-//	}
-//
-//	// Allocate GPU buffers for three vectors (two input, one output)    .
-//	cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaMalloc failed!");
-//		goto Error;
-//	}
-//
-//	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaMalloc failed!");
-//		goto Error;
-//	}
-//
-//	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaMalloc failed!");
-//		goto Error;
-//	}
-//
-//	// Copy input vectors from host memory to GPU buffers.
-//	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaMemcpy failed!");
-//		goto Error;
-//	}
-//
-//	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaMemcpy failed!");
-//		goto Error;
-//	}
-//
-//	// Launch a kernel on the GPU with one thread for each element.
-//	addKernel << <1, size >> > (dev_c, dev_a, dev_b);
-//
-//	// Check for any errors launching the kernel
-//	cudaStatus = cudaGetLastError();
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-//		goto Error;
-//	}
-//
-//	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-//	// any errors encountered during the launch.
-//	cudaStatus = cudaDeviceSynchronize();
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-//		goto Error;
-//	}
-//
-//	// Copy output vector from GPU buffer to host memory.
-//	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-//	if (cudaStatus != cudaSuccess) {
-//		fprintf(stderr, "cudaMemcpy failed!");
-//		goto Error;
-//	}
-//
-//Error:
-//	cudaFree(dev_c);
-//	cudaFree(dev_a);
-//	cudaFree(dev_b);
-//
-//	return cudaStatus;
-//}
+#ifndef BILATERAL_FILTER_CUH_
+#define BILATERAL_FILTER_CUH_
+
+#include<iostream>
+#include<cstdio>
+#include<cuda_runtime.h>
+
+using std::cout;
+using std::endl;
+
+
+void computeGaussianKernelCuda(const float delta, const int radius);
+
+void bilateralFilterCuda(const float4 * const h_input,
+	float4 * const h_output,
+	const float euclidean_delta,
+	const int width, const int height,
+	const int filter_radius);
+
+__constant__ float c_gaussian[64];  
+
+inline void computeGaussianKernelCuda(const float delta, const int radius)
+{
+	float h_gaussian[64];
+	for (int i = 0; i < 2 * radius + 1; ++i)
+	{
+		const float x = i - radius;
+		h_gaussian[i] = expf(-(x * x) / (2.0f * delta * delta));
+	}
+	cudaMemcpyToSymbol(c_gaussian, h_gaussian, sizeof(float)*(2 * radius + 1));
+}
+
+// it computes the euclidean distance between two points, each point a vector with 4 elements
+__device__ inline float euclideanLenCuda(const float4 a, const float4 b, const float d)
+{
+	const float mod = (b.x - a.x) * (b.x - a.x) +
+		(b.y - a.y) * (b.y - a.y) +
+		(b.z - a.z) * (b.z - a.z) +
+		(b.w - a.w) * (b.w - a.w);
+	return expf(-mod / (2.0f * d * d));
+}
+
+__device__ inline float4 multiplyCuda(const float a, const float4 b)
+{
+	return { a * b.x, a * b.y, a * b.z, a * b.w };
+}
+
+__device__ inline float4 addCuda(const float4 a, const float4 b)
+{
+	return { a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w };
+}
+
+__global__ void bilateralFilterCudaKernel(const float4 * const d_input,
+	float4 * const d_output,
+	const float euclidean_delta,
+	const int width, const int height,
+	const int filter_radius)
+{
+	//2D Index of current thread
+	const int x = blockIdx.x * blockDim.x + threadIdx.x;
+	const int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	//Only valid threads perform memory I/O
+	if ((x<width) && (y<height))
+	{
+		float sum = 0.0f;
+		float4 t = { 0.f, 0.f, 0.f, 0.f };
+		const float4 center = d_input[y * width + x];
+		const int r = filter_radius;
+
+		float domainDist = 0.0f, colorDist = 0.0f, factor = 0.0f;
+
+		for (int i = -r; i <= r; ++i)
+		{
+			int crtY = y + i; //clamp the neighbor pixel, prevent overflow
+			if (crtY < 0)				crtY = 0;
+			else if (crtY >= height)   	crtY = height - 1;
+
+			for (int j = -r; j <= r; ++j)
+			{
+				int crtX = x + j;
+				if (crtX < 0) 				crtX = 0;
+				else if (crtX >= width)	 	crtX = width - 1;
+
+				const float4 curPix = d_input[crtY * width + crtX];
+				domainDist = c_gaussian[r + i] * c_gaussian[r + j];
+				colorDist = euclideanLenCuda(curPix, center, euclidean_delta);
+				factor = domainDist * colorDist;
+				sum += factor;
+				t = addCuda(t, multiplyCuda(factor, curPix));
+			}
+		}
+
+		d_output[y * width + x] = multiplyCuda(1.f / sum, t);
+	}
+}
+
+void bilateralFilterCuda(const float4 * const h_input,
+	float4 * const h_output,
+	const float euclidean_delta,
+	const int width, const int height,
+	const int filter_radius)
+{
+	// compute the gaussian kernel for the current radius and delta
+	computeGaussianKernelCuda(euclidean_delta, filter_radius);
+
+	// copy the input image from the CPU´s memory to the GPU´s global memory
+	const int inputBytes = width * height * sizeof(float4);
+	const int outputBytes = inputBytes;
+	float4 *d_input, *d_output; // arrays in the GPU´s global memory
+								// allocate device memory
+	cudaMalloc<float4>(&d_input, inputBytes);
+	cudaMalloc<float4>(&d_output, outputBytes);
+	// copy data of input image to device memory
+	cudaMemcpy(d_input, h_input, inputBytes, cudaMemcpyHostToDevice);
+
+
+
+	// specify a reasonable grid and block sizes
+	const dim3 block(16, 16);
+	// calculate grid size to cover the whole image
+	const dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
+
+	// launch the size conversion kernel
+	bilateralFilterCudaKernel << <grid, block >> >(d_input, d_output, euclidean_delta, width, height, filter_radius);
+
+
+	// synchronize to check for any kernel launch errors
+	cudaDeviceSynchronize();
+
+	//Copy back data from destination device meory to OpenCV output image
+	cudaMemcpy(h_output, d_output, outputBytes, cudaMemcpyDeviceToHost);
+
+	//Free the device memory
+	cudaFree(d_input);
+	(cudaFree(d_output));
+}
+
+#endif /* BILATERAL_FILTER_CUH_ */
--- CudaSample/main.cpp
+++ CudaSample/main.cpp
@@ -1,39 +1,67 @@
-#include <opencv2/opencv.hpp>
+/* *
+* Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
+*
+* Please refer to the NVIDIA end user license agreement (EULA) associated
+* with this source code for terms and conditions that govern your use of
+* this software. Any use, reproduction, disclosure, or distribution of
+* this software and related documentation outside the terms of the EULA
+* is strictly prohibited.
+*/
 #include <iostream>
-#include <fstream>
-#include <vector> // for vector
-#include <iterator> // for std::istream_iterator and std::ostream_iterator
-#include <algorithm> // for std::copy
+#include <stdio.h>
+#include <stdlib.h>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include<cuda_runtime.h>
+
 using namespace std;
 using namespace cv;
 
+void bilateralFilterCuda(const float4 * const h_input,
+	float4 * const h_output,
+	const float euclidean_delta,
+	const int width, const int height,
+	const int filter_radius);
+
+int main(int argc, char **argv) {
 
 
-void readImageOpenCV() {
-	Mat img = imread("flower.jpg");
-	namedWindow("image", WINDOW_NORMAL);
-	imshow("image", img);
-	waitKey(0);
-}
+	cv::Mat input = cv::imread("knowledge_graph_logo.jpeg", IMREAD_UNCHANGED);
 
+	cv::imshow("IMg1", input);
 
-int main() {
+	///convert from char(0-255) BGR to float (0.0-0.1) RGBA
+	Mat inputRGBA;
+	cvtColor(input, inputRGBA, 2, 4);
+	inputRGBA.convertTo(inputRGBA, CV_32FC4);
+	inputRGBA /= 255;
 
-	// Define file stream object, and open the file
-	ifstream inputFile("flower.jpg", ios::binary);
-	ofstream outputFile("flower1.jpg", ios::binary);
+	//Create output image
+	Mat output(input.size(), inputRGBA.type());
+	//Mat output(input.size(), input.type());
 
+	const float euclidean_delta = 3.0f;
+	const int filter_radius = 3;
+/*
+	bilateralFilterCuda((float4*)input.ptr<float4>(),
+		(float4*)output.ptr<float4>(),
+		euclidean_delta,
+		input.cols, input.rows,
+		filter_radius);*/
 
-	// Reading the file content using the iterator!
-	
-	std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(inputFile), {});
-
-	
-	copy(buffer.begin(), buffer.end(), ostreambuf_iterator<char>(outputFile));
-
-
-	cout << "Done";
+	bilateralFilterCuda((float4*)inputRGBA.ptr<float4>(),
+		(float4*)output.ptr<float4>(),
+		euclidean_delta,
+		inputRGBA.cols, inputRGBA.rows,
+		filter_radius);
+	// convert back to char (0-255) BGR
+	output *= 255;
+	//output.convertTo(output, CV_8UC4);
+	cvtColor(output, output, 2, 3);
+	cv::imshow("Img2", output);
+	cv::waitKey();
+	imwrite("output.jpg", output);
 	return 0;
 }
-
-
